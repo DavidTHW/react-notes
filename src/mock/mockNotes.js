05fce4a63eca1ed1e729cd0d2638f51a6e178c4d
HYPERION\David <david.tonghw@gmail.com>
2024-02-23 21:52:26 +0800

    for deployment testing to gh pages

--- src/mock/mockNotes.js
+++ src/mock/mockNotes.js
@@ -1,6 +1,6 @@
 const mockNotes = [
-    {id : 1 , 'username' : 'tester' , 'title' : 'Refresh to see this' , 'content' : 'this is mocked', },
-    {id : 2 , 'username' : 'tester' , 'title' : 'Refresh to see this', 'content' : 'it will always be here'},
+    {id : 1 , 'username' : 'tester' , 'title' : 'Refresh to see this' , 'content' : 'this is mocked , it will always be here , just refresh', },
+    {id : 2 , 'username' : 'tester' , 'title' : 'Refresh to see this', 'content' : 'it will always be here , just refresh'},
     {id : 3 , 'username' : 'tester2' , 'title' : 'Time to break things' ,'content' : 'There is no tester2'},
     {id : 4 , 'username' : 'yoda@jedicouncil.com' , 'title' : 'In this war , A danger there is', 'content' : 'of losing who we are'},
     {id : 5 , 'username' : 'yoda@jedicouncil.com' , 'title' : 'Size matters not' ,'content' : 'looks at me , judge me on my size do you? hmm?'},
